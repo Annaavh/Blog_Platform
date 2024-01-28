2f882969d86eceaadcf3e056128f3a54bc972acc
Anna <annaavetisyan175@gmail.com>
2024-01-28 16:18:56 +0400

    changed format

--- models/BlacklistedTokenModel.js
+++ models/BlacklistedTokenModel.js
@@ -1,8 +1,16 @@
 import mongoose from 'mongoose';
 
 const blacklistedTokenSchema = new mongoose.Schema({
-    token: { type: String, required: true, unique: true },
-    createdAt: { type: Date, default: Date.now, expires: '1d' } 
+    token: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        expires: '1d'
+    }
 });
 
 const BlacklistedToken = mongoose.model('BlacklistedToken', blacklistedTokenSchema);
